06b0526cc34c0f3cded6e32013f1f78b1adb6efd
numselli <58607232+numselli@users.noreply.github.com>
2024-05-01 15:24:19 -0700

    remove what is in my bag

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -54,11 +54,6 @@ const items = [
     icon: "solar:document-add-outline",
   },
   {
-    name: "What's in my bag?",
-    path: "/whats-in-my-bag",
-    icon: "solar:backpack-outline",
-  },
-  {
     name: "Bookmarks",
     path: "/bookmarks",
     icon: "solar:bookmark-linear",
