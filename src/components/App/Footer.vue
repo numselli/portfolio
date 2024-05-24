5278159a3dfdb93d7e1610e66f81c6cb1dc78632
numselli <58607232+numselli@users.noreply.github.com>
2024-05-23 20:45:09 -0700

    Update Footer.vue

--- src/components/App/Footer.vue
+++ src/components/App/Footer.vue
@@ -3,6 +3,10 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
   >
     <br>
-    <p>© 2023 numselli. All rights reserved.</p>
+    <p>© {{year}} numselli. All rights reserved.</p>
   </footer>
 </template>
+
+<script setup>
+const year = new Date().getFullYear()
+</script>
