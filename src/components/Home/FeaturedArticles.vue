476c2fcb7db40a11ebc5fe8abf8479960d6e3dda
numselli <58607232+numselli@users.noreply.github.com>
2025-11-23 00:15:04 -0800

    Update FeaturedArticles.vue

--- src/components/Home/FeaturedArticles.vue
+++ src/components/Home/FeaturedArticles.vue
@@ -3,7 +3,7 @@
     <h2 class="uppercase text-xs font-semibold text-gray-400 mb-6">
       RECENT ARTICLES
     </h2>
-    <UBlogPosts orientation="vertical" class="gap-2 lg:gap-y-4">
+    <UBlogPosts class="gap-2 lg:gap-y-4">
       <UBlogPost v-for="(blog, index) in blogs" :key="index" v-bind="blog" :to="blog.path" :date="blog.date"/>
     </UBlogPosts>
   </div>
